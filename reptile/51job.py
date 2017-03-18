88328238ccf5ac971b6041e0e53f3f9b77d3f8e3
color <color@admin.router>
2017-03-18 10:09:32 +0800

    成功提取

--- reptile/51job.py
+++ reptile/51job.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python 
+#coding=utf8
 _DEBUG=True
 import re
 import urllib
-
-def getHtml():
-	params = urllib.urlencode({'curr_page': 3,'fromJs': 1,'jobarea': 030200,'keyword':'%E8%BF%90%E7%BB%B4','keywordtype': 2,'lang': 'c','stype': 2,'postchannel': 0000,'fromType': 1,'confirmdate': 9})
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+VAR_KEYWORDS='运维'
+VAR_PAGES=1
+def getHtml(pages):
+	params = urllib.urlencode({'curr_page':pages,'fromJs': 1,'jobarea': '030200','keyword':VAR_KEYWORDS,'keywordtype': 2,'lang': 'c','stype': 2,'postchannel': 0000,'fromType': 1,'confirmdate': 9})
+#	print "http://search.51job.com/jobsearch/search_result.php?%s" % params
 	page = urllib.urlopen("http://search.51job.com/jobsearch/search_result.php?%s" % params)
 	html = page.read()
 	return html
@@ -23,17 +29,22 @@ def findurl(html):
         items = re.findall(r'\"([^\"]*)\"',html)
         return items
 def allpages(html):
-        items = re.findall(r'jumpPage\(\'\d{0,5}\'\)',html)
-	items = re.findall(r'\d{,5}',items[0])
+        items = re.findall(r'jumpPage\(\'\d{1,5}\'\)',html)
+	items = re.findall(r'\d{1,5}',items[0])
         return items
-html = getHtml()
+html = getHtml(1)
 html=html.decode('gbk')
 html=html.encode('utf8')
-print html
-print allpages(html)
-ghtml=grep(html)
-for a in ghtml:
-	b=findurl(a)
-	print b[1],b[2]
-
+#print html
+VAR_PAGES=allpages(html)[0]
+pages=int(VAR_PAGES)
+while pages>0:
+	html = getHtml(pages)
+	pages-=1
+	html=html.decode('gbk')
+	html=html.encode('utf8')
+	ghtml=grep(html)
+	for a in ghtml:
+		b=findurl(a)
+		print b[1],b[2]
 
